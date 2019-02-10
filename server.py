a67e1f1f7ca60be38b602493464db58eb525dab4
Hrishikesh Suresh <blackleghubot@github.com>
2019-02-10 14:54:05 +0530

    works alright

--- server.py
+++ server.py
@@ -12,38 +12,37 @@ from werkzeug import secure_filename
 Markup('<h1><strong>Hello!</strong></h1>')
 
 #create application instance
-app = Flask(__name__)
+app = Flask(__name__, template_folder = "templates")
 app.secret_key = os.urandom(16)
 
 #@app.errorhandler(404)
 #def error_404():
 #    print("Not working")
 
-@app.route('/homepage.html')
+@app.route('/homePage.html')
 def home():
     #flash("Works!")
     #print(os.listdir())
-    return render_template('/templates/homepage.html')
-    #return send_from_directory('home')
+    return render_template('homePage.html')
 
 @app.route('/cate1.html')
 def cate1():
     #print(os.listdir())
-    return render_template('/templates/cate1.html')
+    return render_template('cate1.html')
 
 @app.route('/cate2.html')
 def cate2():
     #print(os.listdir())
-    return render_template('/templates/cate2.html')
+    return render_template('cate2.html')
 
 @app.route('/cate3.html')
 def cate3():
     #print(os.listdir())
-    return render_template('/templates/cate3.html')
+    return render_template('cate3.html')
 
 @app.route('/upload.html')
 def upload():
-    return render_template('/templates/upload.html')
+    return render_template('upload.html')
 
 @app.route('/upload/uploadImage', methods = ['POST'])
 def uploadFile():
@@ -52,10 +51,11 @@ def uploadFile():
         f.save(secure_filename(f.filename))
         print('File uploaded successfully')
 
-#with app.test_request_context():
+with app.test_request_context():
     #print(url_for('home', filename = 'home/homePage.css'))
     #print(url_for('home', filename = 'bck.jpeg'))
     #print(url_for('upload', filename = 'upload.html'))
+    url_for('static', filename = "homePage.css")
 
 if __name__ == '__main__':
     app.run(debug = True)
