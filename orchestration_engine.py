be3da00e137b0593a02c56a31b00609e128e18dc
Hrishikesh Suresh <blackleghubot@github.com>
2019-04-27 16:39:25 +0530

    detach keyword added

--- orchestration_engine.py
+++ orchestration_engine.py
@@ -155,8 +155,7 @@ def auto_scaling():
     # one container will start immediately
     # container starts before first incoming requests
     if(act_port_init not in active_ports):
-        print("About to start a new container")
-        docker_client.containers.run("hrishikeshsuresh/acts:latest", ports = {'80' : str(act_port_init)})
+        docker_client.containers.run("hrishikeshsuresh/acts:latest", ports = {'80' : str(act_port_init)}, detach = True)
         ##active_ports.append({act_ports[0] : docker_client.containers.list(limit = 1)})
         active_ports[port_i] = docker_client.containers.list(limit = 1)
         print("First container started. Current active ports ", active_ports)
